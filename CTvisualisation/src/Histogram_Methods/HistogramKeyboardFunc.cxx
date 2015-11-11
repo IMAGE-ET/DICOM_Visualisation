4155fd391645b40957ec9c9bc2fea3000e4b896d
Craig Parkinson <parkcra@gmail.com>
2015-11-11 13:41:19 +0000

    Update comments

--- CTvisualisation/src/Histogram_Methods/HistogramKeyboardFunc.cxx
+++ CTvisualisation/src/Histogram_Methods/HistogramKeyboardFunc.cxx
@@ -3,6 +3,10 @@
 /**
  * @title HistogramKeyboardFunc
  * @description called on keyboard use
+ * 
+ *  Author: craig parkinson
+ *  Organisation: Bangor University
+ *  Supervisor: Franck Vidal
  */
 
 void histogramKeyboardFunc(unsigned char key, int x, int y)
